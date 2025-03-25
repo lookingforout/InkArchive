d4adb4f1ad27a88ed5bdbd97f1326fb81f774bc2
lookingforout <spycrabdad@gmail.com>
2025-03-25 13:30:48 +0200

    sliders

--- src/components/colorsidebar.jsx
+++ src/components/colorsidebar.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './colorsidebar.module.css';
 import ColorWheel from './colorwheel';
 
 const ColorSidebar = () => {
+  const [brushSize, setBrushSize] = useState(10);
+  const [brushOpacity, setBrushOpacity] = useState(100);
+
   return (
     <div className={styles.sidebar}>
       <ColorWheel />
+      <div className={styles.brushSettings}>
+        <div className={styles.setting}>
+          <label>Size:</label>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={brushSize}
+            onChange={(e) => setBrushSize(e.target.value)}
+          />
+          <span>{brushSize}px</span>
+        </div>
+        <div className={styles.setting}>
+          <label>Opacity:</label>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={brushOpacity}
+            onChange={(e) => setBrushOpacity(e.target.value)}
+          />
+          <span>{brushOpacity}%</span>
+        </div>
+      </div>
     </div>
   );
 };
